125236e5946bfb6393d805b13cd490e52a2de378
xTEENx <90169740+xTEENx@users.noreply.github.com>
2024-05-31 00:19:14 +0800

    Add files via upload

---
+++ configs/__init__.py
@@ -0,0 +1,6 @@
+from .basic_config import *
+from .model_config import *
+from .kb_config import *
+from .server_config import *
+from .prompt_config import *
+VERSION = 2.0
---
+++ configs/prompt_config.py
@@ -0,0 +1,56 @@
+# prompt模板使用Jinja2语法，简单点就是用双大括号代替f-string的单大括号
+# 本配置文件支持热加载，修改prompt模板后无需重启服务。
+
+# LLM对话支持的变量：
+#   - input: 用户输入内容
+
+# 知识库和搜索引擎对话支持的变量：
+#   - context: 从检索结果拼接的知识文本
+#   - question: 用户提出的问题
+
+# Agent对话支持的变量：
+
+#   - tools: 可用的工具列表
+#   - tool_names: 可用的工具名称列表
+#   - history: 用户和Agent的对话历史
+#   - input: 用户输入内容
+#   - agent_scratchpad: Agent的思维记录
+
+PROMPT_TEMPLATES = {
+    "llm_chat": {
+        "default":
+            '你现在是西安交通大学的教务机器人，你叫交小通。'
+            '{{ input }}',
+
+        "with_history":
+            'The following is a friendly conversation between a human and an AI. '
+            'The AI is talkative and provides lots of specific details from its context. '
+            'If the AI does not know the answer to a question, it truthfully says it does not know.\n\n'
+            'Current conversation:\n'
+            '{history}\n'
+            'Human: {input}\n'
+            'AI:',
+
+        "py":
+            '你是一个聪明的代码助手，请你给我写出简单的py代码。 \n'
+            '{{ input }}',
+    },
+
+
+    "knowledge_base_chat": {
+        "default":
+            '<指令>你现在是西安交通大学的教务机器人，你叫交小通。根据已知信息，简洁和专业的来回答问题。如果无法从中得到答案，请说 “根据已知信息无法回答该问题”，'
+            '不允许在答案中添加编造成分，答案请使用中文。 </指令>\n'
+            '<已知信息>{{ context }}</已知信息>\n'
+            '<问题>{{ question }}</问题>\n',
+
+        "text":
+            '<指令>你现在是西安交通大学的教务机器人，你叫交小通。根据已知信息，简洁和专业的来回答问题。如果无法从中得到答案，请说 “根据已知信息无法回答该问题”，答案请使用中文。 </指令>\n'
+            '<已知信息>{{ context }}</已知信息>\n'
+            '<问题>{{ question }}</问题>\n',
+
+        "empty":  # 搜不到知识库的时候使用
+            '你现在是西安交通大学的教务机器人，你叫交小通。请你回答我的问题:\n'
+            '{{ question }}\n\n',
+    }
+}
